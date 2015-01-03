c42cd7c453ff0ea6052ce43ad4b6f03f1c1180bb
Manuel Alabor <msites@msites.net>
2015-01-03 19:46:12 +0100

    provide access to activities

--- src/hubs.js
+++ src/hubs.js
@@ -1,14 +1,52 @@
 var app = require('express')()
+	, q = require('q')
 	, discover = new (require('harmonyhubjs-discover'))(61991)
-	, hubs = [];
+	, client = require('harmonyhubjs-client')
+	, hubs = []
+	, clients = {};
+
 
 discover.on('update', function(updatedHubs) {
 	hubs = updatedHubs;
 });
 discover.start();
 
+
+function getClientForHub(hub) {
+	var deferred = q.defer();
+	
+	if(!clients[hub.uuid]) {
+		client(hub.ip)
+			.then(function(client) {
+				clients[hub.uuid] = client;
+				deferred.resolve(client);
+			});
+	} else {
+		deferred.resolve(clients[hub.uuid]);
+	}
+	
+	return deferred.promise;
+}
+
+
 app.get('/', function(req, res) {
 	res.send(hubs);
+});
+
+app.get('/:uuid/activities', function(req, res) {
+	var uuid = req.params.uuid
+		, hub = hubs.filter(function(hub) { return (hub.uuid === uuid); })
+		, client;
+	
+	if(hub.length > 0) { hub = hub[0]; }
+	
+	getClientForHub(hub)
+		.then(function(client) {
+			client.getActivities()
+				.then(function(activities) {
+					res.send(activities);
+				});
+		});
 });
 
 module.exports = app;
