12639f7c35718dbd408daab16863710857cd641f
Manuel Alabor <msites@msites.net>
2015-01-11 22:22:29 +0100

    added api route for getting the current activity id of a hub

--- src/rest/hubs.js
+++ src/rest/hubs.js
@@ -21,6 +21,21 @@ app.get('/:uuid/activities', function(req, res) {
 		});
 });
 
+app.get('/:uuid/activities/current', function(req, res) {
+	debug('get current activity for hub with uuid ' + req.params.uuid);
+
+	var uuid = req.params.uuid
+		, universe = app.get('universe');
+
+	universe.getClientForHubWithUuid(uuid)
+		.then(function(client) {
+			return client.getCurrentActivity()
+		})
+		.then(function(currentActivityId) {
+			res.send({ id: currentActivityId });
+		});
+});
+
 app.post('/:uuid/activities/:id/on', function(req, res) {
 	var uuid = req.params.uuid
 		, id = req.params.id
